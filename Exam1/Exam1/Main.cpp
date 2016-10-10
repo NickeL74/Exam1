5cb9db42057c2f8d246a2b848fe08ad52a53925f
Nicholas Little <nick@LAPTOP-VCD4NI8L>
2016-10-10 14:11:33 -0700

    Basic structure no changes.

---
+++ Exam1/Exam1/Main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+namespace Exam1 // NAMESPACE
+{
+	class Wheel
+	{
+	private:
+		int _radius;
+	public:
+		Wheel(int radius) { _radius = radius; }
+	};
+
+	// STEP 1: Complete the implementation of the Vehicle base class 
+	// as described in the UML diagram
+	class Vehicle
+	{
+	protected:
+		string _color;
+		int _topspeed;
+		vector<Wheel> wheels;
+
+	public:
+		Vehicle(string color) {
+			// Complete the implementation
+		}
+		virtual void Description() = 0;
+	};
+
+	// STEP 2:  Define the RaceCar, Sedan, and Pickup classes as described
+	// in the UML diagram.   For this test, you do not need to separate
+	// the class implementation into a cpp file.
+	class RaceCar {
+		//Top speed: 250mph
+		//Wheel radius: 305mm
+
+	};
+
+	class Sedan {
+		//Top speed: 95mph
+		//Wheel radius: 381mm
+
+	};
+
+	class Pickup {
+		//Top speed: 85mph
+		//Wheel radius: 432mm
+
+	};
+}
+
+
+int main()
+{
+	Exam1::Vehicle * Garage[3];
+
+	// STEP 3: Assign ONE RaceCar, ONE Sedan, AND ONE Pickup object to each of the elements 
+	// in the Garage array.  Remember the classes are defined in the namespace Exam1.
+	//Garage[0] = ???
+	//Garage[1] = ???
+	//Garage[2] = ???
+
+	for (int i = 0; i < 3; ++i)
+	{
+		// The method Description() should display an output like 
+		// "I am a red sedan (or racecar/pickup) and I can go 95 (or 250 or 85) mph"
+		Garage[i]->Description();
+	}
+}
